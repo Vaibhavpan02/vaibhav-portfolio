5d3ab29a4eda4dc47ade56b7855c7cfaa90c6ba5
Vaibhav Pandey <89473295+Vaibhavpan02@users.noreply.github.com>
2024-07-21 23:47:57 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -17,7 +17,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Vaibhavpan02",
   resume:
-    "https://drive.google.com/file/d/12_NUln9WqUJeBB7aTo0tH0yoykuvBxrW/view?usp=drive_link",
+    "https://drive.google.com/file/d/1kCG9U4JFvJI5JKmNI395a4B4iPYZCula/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/vaibhpan",
   // twitter: "https://twitter.com/RishavChanda",
   // insta: "https://www.instagram.com/rishav_chanda/",
